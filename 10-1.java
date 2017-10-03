df798ceac17910c02a0248e11218c30bff94faaa
choihyeonji <chj1768@naver.com>
2017-10-03 18:48:16 +0900

    Initial commit

---
+++ 10-1.java
@@ -0,0 +1,18 @@
+10-1 MinPerimeterRectangle
+Find the minimal perimeter of any rectangle whose area equals N.
+
+//https://codility.com/demo/results/trainingZZZ5MT-KX6/
+
+class Solution {
+    public int solution(int N) {
+        int i = 1;
+        int min = ( 1 + N )*2;
+        while( i*i <= N ) {
+            if ( N%i == 0 ) {
+                min = min > ( N/i + i )*2 ? ( N/i + i )*2 : min;
+            }
+            i++;
+        }
+        return min;
+    }
+}
